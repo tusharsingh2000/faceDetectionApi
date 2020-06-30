53b762ecf992a6d858ead757be2340b160b66447
tusharsingh2000 <tusharsinghynr@gmail.com>
2020-06-30 13:18:00 +0530

    updated port

--- server.js
+++ server.js
@@ -54,6 +54,6 @@ app.get('/profile/:id', (req,res)=> {profile.handleProfile(req,res,db)});
 app.put('/image' ,(req,res)=> {image.handleImage(req,res,db)});
 app.post('/imageUrl' ,(req,res)=> {image.handleApiCall(req,res)});
 
-app.listen(3000,()=>{
-    console.log('App is listening')
+app.listen(process.env.PORT || 3000,()=>{
+    console.log(`App is listening ${process.env.PORT}`)
 })
